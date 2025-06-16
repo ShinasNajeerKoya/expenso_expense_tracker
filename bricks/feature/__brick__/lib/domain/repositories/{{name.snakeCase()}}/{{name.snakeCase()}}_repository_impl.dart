17a5ef15d5c949874a581f136f1c5fde5a622cdf
shinasKoya <shinasnajeerkoya@gmail.com>
2025-06-16 18:26:41 +0530

    -- bricks folder updated to git

---
+++ bricks/feature/__brick__/lib/domain/repositories/{{name.snakeCase()}}/{{name.snakeCase()}}_repository_impl.dart
@@ -0,0 +1,8 @@
+import 'package:expenso_expense_tracker/domain/repositories/{{name.snakeCase()}}/{{name.snakeCase()}}_repository.dart';
+
+class {{name.pascalCase()}}RepositoryImpl implements {{name.pascalCase()}}Repository {
+  @override
+  Future<void> start{{name.pascalCase()}}() async {
+    // start your repository here
+  }
+}
